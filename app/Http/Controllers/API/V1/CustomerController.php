88b03a0c77f73c2141ef348a1ba050ccf5b1cc55
sayma-code <saymaobaida2@gmail.com>
2024-08-07 02:38:16 +0600

    resource

--- app/Http/Controllers/API/V1/CustomerController.php
+++ app/Http/Controllers/API/V1/CustomerController.php
@@ -19,13 +19,17 @@ class CustomerController extends Controller
     public function index(Request $request)
     {
         $filter = new CustomersFilter();
-        $queryItems = $filter->transform($request); // [column, operator, value]
+        $filterItems = $filter->transform($request); // [column, operator, value]
 
-        if (count($queryItems) == 0) {
-            return new CustomerCollection(Customer::paginate()) ;
-        }else{
-            return new CustomerCollection(Customer::where($queryItems)->paginate()) ;
+        $includeInvoices = $request->query('includeInvoices');
+
+        $customers = Customer::where($filterItems);
+
+        if($includeInvoices) {
+            $customers = $customers->with('invoices');
         }
+
+        return new CustomerCollection($customers->paginate()->appends($request->query())) ;
     }
 
     /**
